2bcf71ad9865811dc779ca5eba62b048bbe3080e
Vitor de Miranda Henrique <vitormhenrique@gmail.com>
2019-01-22 10:34:58 -0600

    fixing imports
    fixing imports

--- examples/ra8875_performance_test.py
+++ examples/ra8875_performance_test.py
@@ -1,8 +1,10 @@
 import time
-import machine
-import micropython_ra8875.registers as reg
-import micropython_ra8875.ra8875 as ra8875
-from micropython_ra8875.ra8875 import color565
+import busio
+import digitalio
+import board
+
+import adafruit_ra8875.ra8875 as ra8875
+from adafruit_ra8875.ra8875 import color565
 
 BLACK = color565(0, 0, 0)
 RED = color565(255, 0, 0)
